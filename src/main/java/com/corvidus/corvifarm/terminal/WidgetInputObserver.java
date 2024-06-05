a2ab6b9946cfa3ee55ff1adf6e5943a881eb54bf
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-05 17:18:03 +0200

    Removed hasFilter.

--- src/main/java/com/corvidus/corvifarm/terminal/WidgetInputObserver.java
+++ src/main/java/com/corvidus/corvifarm/terminal/WidgetInputObserver.java
@@ -3,6 +3,5 @@ package com.corvidus.corvifarm.terminal;
 import com.googlecode.lanterna.input.KeyStroke;
 
 public interface WidgetInputObserver {
-	public boolean hasFilter();
 	public void onInput(TerminalWidget widget, KeyStroke keyStroke);
 }

aade8ea78171933f51fd5280ce528c7bd6c31e56
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-06 21:56:07 +0200

    Added Exception which is to be thrown on actions the player is unable to perform

---
+++ src/main/java/com/corvidus/corvifarm/game/InvalidActionException.java
@@ -0,0 +1,9 @@
+package com.corvidus.corvifarm.game;
+/**
+ * Invalid actions are actions by the player which he is not able to do, like
+ * using a wrong tool, being out of energy, being out of gold and so on.
+ * @author hm
+ */
+public class InvalidActionException extends Exception {
+	
+}

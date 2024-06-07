1b7767b378b9f0ea865db5422fb1d52bc9316b5d
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-07 22:02:57 +0200

    WASDSelect displays first n fields and selects the 1st one per default.
    No paging/selection implemented yet.

--- src/main/java/com/corvidus/corvifarm/terminal/select/WASDSelect.java
+++ src/main/java/com/corvidus/corvifarm/terminal/select/WASDSelect.java
@@ -1,19 +1,83 @@
 package com.corvidus.corvifarm.terminal.select;
 
 import com.corvidus.corvifarm.terminal.WidgetAbstract;
+import com.corvidus.corvifarm.terminal.WidgetString;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.input.KeyStroke;
+import java.util.ArrayList;
 
 public class WASDSelect extends WidgetAbstract {
-	public WASDSelect(int posX, int posY, int width, int height) {
+	private int colwidth;
+	private int visible = 0;
+	private WidgetString[] strWidgets;
+	private ArrayList<WASDSelectElement> elements = new ArrayList<>();
+	//private ArrayList<WidgetString> strWidgets = new ArrayList<>();
+	public WASDSelect(int posX, int posY, int width, int height, int colwidth) {
 		super(posX, posY, width, height);
+		this.colwidth = colwidth;
+		int wideWidth = colwidth + 3;
+		/*
+		 * WideWith is colWidth + 3: [* CONTENT ]. The last column is one character
+		 * too wide, because it does not need the blank to separate itself from
+		 * the next column. Hence, the width is extended by this additional
+		 * blank to get a correct result for "how many columns of width w fit into
+		 * total width tw."
+		 */
+		int columns = Math.floorDiv(this.getWidth()+1, wideWidth);
+		int rows = this.getHeight();
+		this.visible = rows*columns;
+		this.strWidgets = new WidgetString[this.visible];
+		int k = 0;
+		/**
+		 * Loop to create the maximum amount of fields which can be visible on
+		 * the screen at one time.
+		 */
+		for(int col = 0; col<columns;col++) {
+			for(int row = 0;row < this.getHeight(); row++) {
+				this.strWidgets[k] = new WidgetString((col*wideWidth), row, wideWidth);
+				if(k == 0) {
+					this.strWidgets[k].setString("*");
+				}
+				k++;
+			}
+		}
 	}
 	
-	public void addElement() {
-		
+	public void addElement(WASDSelectElement element) {
+		this.elements.add(element);
 	}
 	
 	@Override
 	public void onInput(KeyStroke keyStroke) {
 		
+	}
+	
+	private void rewriteStrings(TextGraphics th) {
+		for(int i = 0; i<this.strWidgets.length;i++) {
+			WidgetString strWidget = this.strWidgets[i];
+			String elementName = "";
+			try {
+				elementName = this.elements.get(i).getWASDString();
+			} catch (IndexOutOfBoundsException e) {
+				
+			}
+			
+			if(i == 0) {
+				strWidget.setString("* "+elementName);
+			} else {
+				strWidget.setString("  "+elementName);
+			}
+			tg.drawImage(new TerminalPosition(strWidget.getPosX(), strWidget.getPosY()), strWidget.getTextImage());
+		}
+	}
+	
+	@Override
+	public TextImage getTextImage() {
+		TextImage ti = super.getTextImage();
+		TextGraphics tg = ti.newTextGraphics();
+		this.rewriteStrings(tg);
+	return ti;
 	}
 }
--- src/test/java/com/corvidus/corvifarm/terminal/select/WASDSelectTest.java
+++ src/test/java/com/corvidus/corvifarm/terminal/select/WASDSelectTest.java
@@ -1,8 +1,13 @@
 package com.corvidus.corvifarm.terminal.select;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.graphics.BasicTextImage;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.input.KeyStroke;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,6 +22,15 @@ public class WASDSelectTest {
 	public WASDSelectTest() {
 	}
 	
+	public WASDSelect createDefault() {
+		WASDSelect wasd = new WASDSelect(0, 0, 23, 3, 5);
+		for(int i = 0;i<80;i++) {
+			WASDSelectString element = new WASDSelectString(String.format("TST%02d", i));
+			wasd.addElement(element);
+		}
+	return wasd;
+	}
+		
 	@BeforeAll
 	public static void setUpClass() {
 	}
@@ -34,31 +48,28 @@ public class WASDSelectTest {
 	}
 
 	/**
-	 * Test of addElement method, of class WASDSelect.
+	 * Test constructor, get empty image.
 	 */
 	@Test
-	public void testAddElement() {
-		/*
-		System.out.println("addElement");
-		WASDSelect instance = null;
-		instance.addElement();
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-		*/
+	public void testConstructEmpty() {
+		WASDSelect wasd = new WASDSelect(0, 0, 23, 3, 5);
+		TextImage ti = new BasicTextImage(23, 3);
+		TextGraphics tg = ti.newTextGraphics();
+		tg.putString(0, 0, "*");
+		Assertions.assertEquals(ti.toString(), wasd.getTextImage().toString());
 	}
 
 	/**
 	 * Test of onInput method, of class WASDSelect.
 	 */
 	@Test
-	public void testOnInput() {
-		/*
-		System.out.println("onInput");
-		KeyStroke keyStroke = null;
-		WASDSelect instance = null;
-		instance.onInput(keyStroke);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-		*/
+	public void testConstructFilled() {
+		WASDSelect wasd = this.createDefault();
+		TextImage ti = new BasicTextImage(23, 3);
+		TextGraphics tg = ti.newTextGraphics();
+		tg.putString(0, 0, "* TST00   TST03   TST06");
+		tg.putString(0, 1, "  TST01   TST04   TST07");
+		tg.putString(0, 2, "  TST02   TST05   TST08");
+		Assertions.assertEquals(ti.toString(), wasd.getTextImage().toString());
 	}
 }

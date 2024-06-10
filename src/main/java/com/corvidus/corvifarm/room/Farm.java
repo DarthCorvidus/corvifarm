447fb6ff1df35b6acfd01f4b5a0dd4da278a17ee
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-10 22:07:53 +0200

    Farm.init adds wood as first example for an overlay.
    Using a HashMap to create WASDSelectGroup instances to which tiles are added.

--- src/main/java/com/corvidus/corvifarm/room/Farm.java
+++ src/main/java/com/corvidus/corvifarm/room/Farm.java
@@ -1,17 +1,40 @@
 package com.corvidus.corvifarm.room;
 
 import com.corvidus.corvifarm.game.Calendar;
+import com.corvidus.corvifarm.items.wood.Wood;
 import com.corvidus.corvifarm.terminal.select.WASDSelectElement;
 import com.corvidus.corvifarm.terminal.select.WASDSelectString;
+import com.corvidus.corvifarm.tiles.Tile;
+import com.corvidus.corvifarm.tiles.FarmTile;
+import com.corvidus.corvifarm.tiles.WASDSelectGroup;
+import java.util.HashMap;
 public class Farm extends RoomAbstract {
 	private Farm() {
 		super();
 	}
 	
 	private void init() {
-		this.wasd.addElement(new WASDSelectString("15×Oak Tree"));
-		this.wasd.addElement(new WASDSelectString("17×Maple Tree"));
-		this.wasd.addElement(new WASDSelectString("28×Stone"));
+		HashMap<String, WASDSelectGroup> group = new HashMap<>();
+		for(int i = 0;i<4096;i++) {
+			Tile tile = new FarmTile();
+			this.tiles.add(tile);
+			/*
+			 * Spawn Wood.
+			*/
+			if(this.rand.nextInt(10)<1) {
+				tile.setOverlay(new Wood());
+				continue;
+			}
+		}
+		for(Tile tile : this.tiles) {
+			if(!group.containsKey(tile.getName())) {
+				group.put(tile.getName(), new WASDSelectGroup());
+			}
+			group.get(tile.getName()).addTile(tile);
+		}
+		for(String key : group.keySet()) {
+			this.wasd.addElement(group.get(key));
+		}
 	}
 	
 	public static Farm fromScratch() {

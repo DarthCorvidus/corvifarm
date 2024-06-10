0cd7861eae6b506f36231bb1648f9c9361949f08
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-10 21:59:57 +0200

    This class will be used to group tiles of the same type in WASDSelect.
    Once onSelect is called, the first tile of each group will be returned.

---
+++ src/main/java/com/corvidus/corvifarm/tiles/WASDSelectGroup.java
@@ -0,0 +1,24 @@
+package com.corvidus.corvifarm.tiles;
+
+import com.corvidus.corvifarm.terminal.select.WASDSelectElement;
+import java.util.ArrayList;
+public class WASDSelectGroup implements WASDSelectElement {
+	private final ArrayList<Tile> tiles = new ArrayList<>();
+	public void addTile(Tile tile) {
+		this.tiles.add(tile);
+	}
+	
+	@Override
+	public String getWASDString() {
+		if(this.tiles.isEmpty()) {
+			return "INVALID";
+		}
+	return this.tiles.size()+"×"+this.tiles.get(0).getName();
+	}
+
+	@Override
+	public Object getObject() {
+		return this.tiles.get(0);
+	}
+	
+}

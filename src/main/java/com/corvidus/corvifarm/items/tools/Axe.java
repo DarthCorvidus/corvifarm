1a8c6122807c580d1dfabd9fe6cb70f5f3a16646
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-12 19:58:18 +0200

    Added standard tools, with no functionality.

---
+++ src/main/java/com/corvidus/corvifarm/items/tools/Axe.java
@@ -0,0 +1,15 @@
+package com.corvidus.corvifarm.items.tools;
+
+import com.corvidus.corvifarm.items.ItemAbstract;
+public class Axe extends ItemAbstract {
+	@Override
+	public int getBaseDemand() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "Axe";
+	}
+	
+}
---
+++ src/main/java/com/corvidus/corvifarm/items/tools/Hoe.java
@@ -0,0 +1,15 @@
+package com.corvidus.corvifarm.items.tools;
+
+import com.corvidus.corvifarm.items.ItemAbstract;
+public class Hoe extends ItemAbstract {
+	@Override
+	public int getBaseDemand() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "Hoe";
+	}
+	
+}

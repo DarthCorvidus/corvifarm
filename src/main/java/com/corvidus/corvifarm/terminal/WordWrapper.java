0ac78761dde3ffcae81e36c8906ddf6bc7f74200
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-12 17:11:03 +0200

    Simple word wrapper for use in other widgets.

---
+++ src/main/java/com/corvidus/corvifarm/terminal/WordWrapper.java
@@ -0,0 +1,55 @@
+package com.corvidus.corvifarm.terminal;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordWrapper {
+	private int width = 0;
+	private List<String> lines = new ArrayList<>();
+	public WordWrapper(String string, int width) {
+		this.width = width;
+		String[] split = string.split("\n");
+		for(String line: split) {
+			this.wrapLine(line);
+		}
+	}
+	
+	public WordWrapper(Path path, int width) throws IOException {
+		this.width = width;
+		List<String> lines;
+        try {
+            lines = Files.readAllLines(path);
+			this.wrapText(lines);
+        } catch (IOException e) {
+            throw e;
+        }
+	}
+	
+	private void wrapText(List<String> lines) {
+		for(String line : lines) {
+			this.wrapLine(line.trim());
+		}
+	}
+	
+	private void wrapLine(String line) {
+		String[] split = line.split(" ");
+		String tmp = "";
+		for(String word : split) {
+			if(tmp.length() + word.length()<this.width) {
+				tmp += word+" ";
+			} else {
+				tmp = tmp.trim();
+				this.lines.add(tmp);
+				tmp = word+" ";
+			}
+		}
+		this.lines.add(tmp);
+	}
+	
+	public List<String> getLines() {
+		return this.lines;
+	}
+}

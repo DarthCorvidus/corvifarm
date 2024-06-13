5afbac1d34db4421526e3751433a124308f900c0
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-13 18:19:06 +0200

    First true interaction: Axe chops down Wood on the ground.

--- src/main/java/com/corvidus/corvifarm/items/tools/Axe.java
+++ src/main/java/com/corvidus/corvifarm/items/tools/Axe.java
@@ -1,7 +1,14 @@
 package com.corvidus.corvifarm.items.tools;
 
+import com.corvidus.corvifarm.game.InvalidActionException;
+import com.corvidus.corvifarm.game.Player;
+import com.corvidus.corvifarm.items.Axable;
+import com.corvidus.corvifarm.items.Item;
 import com.corvidus.corvifarm.items.ItemAbstract;
-public class Axe extends ItemAbstract {
+import com.corvidus.corvifarm.items.TileManipulator;
+import com.corvidus.corvifarm.tiles.Tile;
+import java.util.List;
+public class Axe extends ItemAbstract implements TileManipulator {
 	@Override
 	public int getBaseDemand() {
 		return 0;
@@ -10,6 +17,25 @@ public class Axe extends ItemAbstract {
 	@Override
 	public String getName() {
 		return "Axe";
+	}
+
+	@Override
+	public void apply(Player player, Tile tile) throws InvalidActionException {
+		if(!tile.hasOverlay()) {
+			throw new InvalidActionException("Nothing to chop down here.");
+		}
+		Item item = tile.getOverlay();
+		if(!(item instanceof Axable)) {
+			throw new InvalidActionException("Object cannot be chopped down");
+		}
+		Axable axable = (Axable)item;
+		axable.axe();
+		if(!axable.yieldsItem()) {
+			return;
+		}
+		List<Item> items = axable.getAxedItems();
+		player.getInventory().addItems(items);
+		tile.removeOverlay();
 	}
 	
 }

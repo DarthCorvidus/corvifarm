7f77cf1b9c1898c10b0cc1838b69970da15bac33
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-15 19:53:03 +0200

    Bed now asks if you want to sleep. Just as in real life.

--- src/main/java/com/corvidus/corvifarm/items/interactive/Bed.java
+++ src/main/java/com/corvidus/corvifarm/items/interactive/Bed.java
@@ -3,11 +3,21 @@ package com.corvidus.corvifarm.items.interactive;
 import com.corvidus.corvifarm.game.Game;
 import com.corvidus.corvifarm.items.Interactive;
 import com.corvidus.corvifarm.items.ItemAbstract;
+import com.corvidus.corvifarm.terminal.UserInterface;
+import com.corvidus.corvifarm.terminal.yesno.WidgetYesNo;
+import com.corvidus.corvifarm.terminal.yesno.YesNoObserver;
 
-public class Bed extends ItemAbstract implements Interactive {
+public class Bed extends ItemAbstract implements Interactive, YesNoObserver {
+	private Game game;
 	@Override
 	public void interact(Game game) {
-		game.calendar.sleep();
+		this.game = game;
+		WidgetYesNo yesNo = new WidgetYesNo(10, 5, 20, 4, "Sleep? (y/N)");
+		yesNo.addYesNoObserver(this);
+		this.game.calendar.pause();
+		this.game.userInterface.setFocus(yesNo);
+
+		//game.calendar.sleep();
 	}
 
 	@Override
@@ -18,5 +28,24 @@ public class Bed extends ItemAbstract implements Interactive {
 	@Override
 	public String getName() {
 		return "Bed";
+	}
+
+	@Override
+	public void onYes(WidgetYesNo widgetYesNo) {
+		this.game.calendar.sleep();
+		this.game.userInterface.removeWidget(widgetYesNo);
+		this.game.calendar.resume();
+	}
+
+	@Override
+	public void onNo(WidgetYesNo widgetYesNo) {
+		this.game.userInterface.removeWidget(widgetYesNo);
+		this.game.calendar.resume();
+	}
+
+	@Override
+	public void onInvalid(WidgetYesNo widgetYesNo) {
+		this.game.userInterface.removeWidget(widgetYesNo);
+		this.game.calendar.resume();
 	}
 }

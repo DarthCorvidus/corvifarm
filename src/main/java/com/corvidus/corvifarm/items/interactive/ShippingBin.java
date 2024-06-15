ed13c041fec1d597565da36b34415b75b2fb4485
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-15 20:21:59 +0200

    Added ShippingBin (currently immediate payout)

---
+++ src/main/java/com/corvidus/corvifarm/items/interactive/ShippingBin.java
@@ -0,0 +1,39 @@
+package com.corvidus.corvifarm.items.interactive;
+
+import com.corvidus.corvifarm.game.Game;
+import com.corvidus.corvifarm.game.InvalidActionException;
+import com.corvidus.corvifarm.game.Inventory;
+import com.corvidus.corvifarm.game.Player;
+import com.corvidus.corvifarm.items.Interactive;
+import com.corvidus.corvifarm.items.Item;
+import com.corvidus.corvifarm.items.ItemAbstract;
+
+public class ShippingBin extends ItemAbstract implements Interactive {
+
+	@Override
+	public int getBaseDemand() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "Shipping Bin";
+	}
+
+	@Override
+	public void interact(Game game) throws InvalidActionException {
+		Player player = game.getPlayer();
+		Inventory inv = game.getPlayer().getInventory();
+		try {
+			Item item = inv.getCurrentItem();
+			if(item.getBaseDemand() == 0) {
+				throw new InvalidActionException("Item cannot be sold.");
+			}
+			inv.subCurrentItem();
+			player.addGold(item.getBaseDemand());
+		} catch (IndexOutOfBoundsException e) {
+			return;
+		}
+		
+	}
+}

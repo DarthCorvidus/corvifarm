c52374d2a06b0e8771924d235030a32c0d46cfe0
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-16 16:00:39 +0200

    Turned Crops into a Singleton.

--- src/main/java/com/corvidus/corvifarm/items/crops/Crops.java
+++ src/main/java/com/corvidus/corvifarm/items/crops/Crops.java
@@ -9,33 +9,36 @@ public class Crops {
 	public static final int WHEAT = 0;
 	public static final int PARSNIP = 1;
 	public static final int POTATO = 2;
-	Map<Integer, Crop> crops;
+	private static Crops crops;
+	Map<Integer, Crop> prototypes;
 	private Crops() {
-		this.crops = new HashMap<>();
+		this.prototypes = new HashMap<>();
 		this.addPrototype(new Crop(WHEAT, 4, 10, "Wheat"));
 		this.addPrototype(new Crop(PARSNIP, 4, 10, "Parsnip"));
 		this.addPrototype(new Crop(POTATO, 6, 40, "Potato"));
 	}
 	
 	private void addPrototype(Crop crop) {
-		this.crops.put(crop.getID(), crop);
+		this.prototypes.put(crop.getID(), crop);
+	}
+	
+	public static Crops getInstance() {
+		if(Crops.crops == null) {
+			Crops.crops = new Crops();
+		}
+	return crops;
 	}
 	
 	public static List<Crop> getPrototypes() {
-		Crops crops = new Crops();
 		ArrayList<Crop> templates = new ArrayList<>();
-		for(int key : crops.crops.keySet()) {
-			templates.add(Crops.createSeed(crops.crops.get(key).getID()));
+		for(int key : Crops.getInstance().prototypes.keySet()) {
+			templates.add(Crops.createSeed(crops.prototypes.get(key).getID()));
 		}
 	return templates;
 	}
 	
 	public static Crop create(int id) {
-		/**
-		 * This is not correct, have to use a singleton here.
-		 */
-		Crops crops = new Crops();
-		Crop proto = crops.crops.get(id);
+		Crop proto = Crops.getInstance().prototypes.get(id);
 		Crop inst = proto.create();
 	return inst;
 	}

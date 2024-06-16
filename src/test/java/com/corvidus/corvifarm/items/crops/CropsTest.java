f5d43fe1eab8cb42d1c387fd528977ff0fa36978
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-16 15:34:33 +0200

    Short test for crops.

---
+++ src/test/java/com/corvidus/corvifarm/items/crops/CropsTest.java
@@ -0,0 +1,35 @@
+package com.corvidus.corvifarm.items.crops;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CropsTest {
+
+	public CropsTest() {
+	}
+	
+	/**
+	 * Test of getPrototypes method, of class Crops.
+	 */
+	@Test
+	public void testGetPrototypes() {
+		List<Crop> prototypes = Crops.getPrototypes();
+		for(Crop crop: prototypes) {
+			assertSame(crop.getState(), Crop.SEED);
+		}
+	}
+	
+	public void testCreateSeed() {
+		Crop crop = Crops.createSeed(Crops.WHEAT);
+		assertSame(Crops.WHEAT, crop.getID());
+		assertSame(Crop.SEED, crop.getState());
+	}
+	
+	public void testCreateProduce() {
+		Crop crop = Crops.createProduce(Crops.PARSNIP);
+		assertSame(Crops.PARSNIP, crop.getID());
+		assertSame(Crop.PRODUCE, crop.getState());
+	}
+
+}

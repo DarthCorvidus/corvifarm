18cba6e51bf7e7b8bf069a6e15a37d89d0cf940f
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-19 17:14:16 +0200

    Added ForestTile and GroundTile
    GroundTiles are tillable to look for treasures.
    ForestTiles will regrow trees planted on them (not yet implemented).

---
+++ src/main/java/com/corvidus/corvifarm/tiles/ForestTile.java
@@ -0,0 +1,12 @@
+package com.corvidus.corvifarm.tiles;
+public class ForestTile extends TileAbstract {
+	@Override
+	public void passDay() {
+		
+	}
+
+	@Override
+	public String getName() {
+		return "Forest floor";
+	}
+}
---
+++ src/main/java/com/corvidus/corvifarm/tiles/GroundTile.java
@@ -0,0 +1,29 @@
+package com.corvidus.corvifarm.tiles;
+
+import com.corvidus.corvifarm.game.InvalidActionException;
+
+public class GroundTile extends TileAbstract implements Tillable {
+	private boolean tilled = false;
+	@Override
+	public void passDay() {
+		this.tilled = false;
+	}
+
+	@Override
+	public String getName() {
+		if(this.tilled) {
+			return "Tilled ground";
+		}
+		return "Empty ground";
+	}
+
+	@Override
+	public void till() throws InvalidActionException {
+		this.tilled = true;
+	}
+
+	@Override
+	public boolean isTilled() {
+		return this.tilled;
+	}
+}

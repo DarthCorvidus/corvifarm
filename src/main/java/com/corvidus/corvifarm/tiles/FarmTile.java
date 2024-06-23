0d23a662e5507ff493d7cf6ecc808333508518f2
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-23 21:51:21 +0200

    Added the wrong file in the last commit. This is the one I meant.

--- src/main/java/com/corvidus/corvifarm/tiles/FarmTile.java
+++ src/main/java/com/corvidus/corvifarm/tiles/FarmTile.java
@@ -5,29 +5,31 @@ import com.corvidus.corvifarm.items.Daily;
 import com.corvidus.corvifarm.items.crops.Crop;
 import java.util.Random;
 public class FarmTile extends TileAbstract implements Tillable, Waterable {
-	private boolean tilled = false;
-	private boolean watered = false;
+	private byte tilledWatered = 0;
+	public static byte FLAT = 0;
+	public static byte TILLED = 1;
+	public static byte WATERED = 2;
 	private final Random rand = new Random();
 	@Override
 	public void till() throws InvalidActionException {
-		if(this.tilled) {
+		if(this.tilledWatered == FarmTile.TILLED) {
 			throw new InvalidActionException("Already tilled.");
 		}
 		if(this.hasOverlay()) {
 			throw new InvalidActionException("Cannot till used tile.");
 		}
-		this.tilled = true;
+		this.tilledWatered = FarmTile.TILLED;
 	}
 
 	@Override
 	public void water() throws InvalidActionException {
-		if(!this.tilled) {
+		if(this.tilledWatered == FarmTile.FLAT) {
 			throw new InvalidActionException("Cannot water untilled tile.");
 		}
-		if(this.watered) {
+		if(this.tilledWatered == FarmTile.WATERED) {
 			throw new InvalidActionException("Already watered.");
 		}
-		this.watered = true;
+		this.tilledWatered = FarmTile.WATERED;
 	}
 
 	@Override
@@ -36,26 +38,24 @@ public class FarmTile extends TileAbstract implements Tillable, Waterable {
 			Daily daily = (Daily)this.getOverlay();
 			daily.passDay(this);
 		}
-		if(this.watered) {
-			this.watered = false;
-		}
-		if(this.tilled && this.rand.nextInt(10)<5 && !this.hasOverlay()) {
-			this.tilled = false;
+		this.tilledWatered = FarmTile.TILLED;
+		if(this.rand.nextInt(10)<5 && !this.hasOverlay()) {
+			this.tilledWatered = FarmTile.FLAT;
 		}
 	}
 	
 	@Override
 	public String getName() {
-		if(this.hasOverlay() && this.getOverlay() instanceof Crop && !this.watered) {
+		if(this.hasOverlay() && this.getOverlay() instanceof Crop && this.tilledWatered == FarmTile.TILLED) {
 			return "!"+this.getOverlay().getName();
 		}
 		if(this.hasOverlay()) {
 			return this.getOverlay().getName();
 		}
-		if(!this.tilled) {
+		if(this.tilledWatered == FarmTile.FLAT) {
 			return "Untilled";
 		}
-		if(!this.watered) {
+		if(this.tilledWatered == FarmTile.TILLED) {
 			return "Unwatered";
 		}
 	return "Watered";
@@ -63,37 +63,23 @@ public class FarmTile extends TileAbstract implements Tillable, Waterable {
 	
 	@Override
 	public boolean isTilled() {
-		return this.tilled;
+		return this.tilledWatered >= FarmTile.TILLED;
 	}
 	
 	@Override
 	public boolean isWatered() {
-		return this.watered;
+		return this.tilledWatered == FarmTile.WATERED;
 	}
 
 	@Override
-	public void setModifiers(byte a, byte b, byte c, byte d) {
-		if(a == 2) {
-			this.tilled = true;
-			this.watered = true;
-		}
-		if(a == 1) {
-			this.tilled = true;
-		}
+	public void setModifiers(byte tilledWatered, byte b, byte c, byte d) {
+		this.tilledWatered = tilledWatered;
 	}
 	
 	@Override
 	public byte[] getModifiers() {
 		byte[] mods = {0, 0, 0, 0};
-		if(this.tilled) {
-			mods[0] = 1;
-		}
-		if(this.watered) {
-			mods[0] = 2;
-		}
-		for(byte b : mods) {
-			System.out.println("Byte "+b);
-		}
+		mods[0] = this.tilledWatered;
 	return mods;
 	}
 	

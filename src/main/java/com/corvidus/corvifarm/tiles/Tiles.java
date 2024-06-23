10125ad045e2050e70ff072a6137ee726f36ee9b
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-23 21:48:16 +0200

    Replaced the two boolean values tilled/watered by a byte tilledWatered
    This will be stored to and loaded from a binary file.

---
+++ src/main/java/com/corvidus/corvifarm/tiles/Tiles.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.corvidus.corvifarm.tiles;
+
+import java.util.List;
+
+/**
+ *
+ * @author hm
+ */
+/*
+public class Tiles {
+	public Tiles() {
+		
+	}
+	
+	public Tiles(List<Tiles> tiles) {
+		
+	}
+	
+	public void add(int id) {
+		
+	}
+	
+	public void remove(int id) {
+		
+	}
+	
+	public int getCount() {
+		
+	}
+	
+	public boolean hasTile(int id) {
+		
+	}
+	
+	public Tile getEntry(int id) {
+		
+	}
+}
+*/

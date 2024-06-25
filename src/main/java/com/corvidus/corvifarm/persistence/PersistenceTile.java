16fa5b76a7bb3b1aff1607c254281b590939ce52
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-25 21:32:05 +0200

    Persistence for tiles.

---
+++ src/main/java/com/corvidus/corvifarm/persistence/PersistenceTile.java
@@ -0,0 +1,50 @@
+package com.corvidus.corvifarm.persistence;
+
+import com.corvidus.corvifarm.tiles.Tile;
+import com.corvidus.corvifarm.tiles.TileFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class PersistenceTile {
+	private int tileId;
+	private byte[] modifiers;
+	private int roomId;
+	private PersistenceTile() {
+		
+	}
+	
+	public static PersistenceTile fromTile(Tile tile, int roomId) {
+		PersistenceTile et = new PersistenceTile();
+		et.tileId = tile.getId();
+		et.modifiers = tile.getModifiers();
+		et.roomId = roomId;
+	return et;
+	}
+	
+	public Tile createTile() {
+		Tile tile = TileFactory.getPrototype(this.tileId);
+		tile.setModifiers(this.modifiers[0], this.modifiers[1], this.modifiers[2], this.modifiers[3]);
+	return tile;
+	}
+	
+	public void toBinary(DataOutputStream dos) throws IOException {
+		dos.writeShort(tileId);
+		for(int i = 0; i<4; i++) {
+			dos.writeByte(this.modifiers[i]);
+		}
+		dos.writeShort(this.roomId);
+	}
+	
+	public static PersistenceTile fromBinary(DataInputStream dis) throws IOException {
+		PersistenceTile pt = new PersistenceTile();
+		pt.tileId = dis.readShort();
+		pt.modifiers = new byte[4];
+		for(int i = 0; i<4; i++) {
+			pt.modifiers[i] = dis.readByte();
+		}
+		pt.roomId = dis.readShort();
+	return pt;
+	}
+	
+}
---
+++ src/test/java/com/corvidus/corvifarm/persistence/PersistenceTileTest.java
@@ -0,0 +1,48 @@
+package com.corvidus.corvifarm.persistence;
+
+import com.corvidus.corvifarm.items.ItemFactory;
+import com.corvidus.corvifarm.items.crops.Crop;
+import com.corvidus.corvifarm.tiles.FarmTile;
+import com.corvidus.corvifarm.tiles.Tile;
+import com.corvidus.corvifarm.tiles.TileFactory;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class PersistenceTileTest {
+	@Test
+	public void testToBinary() throws Exception {
+		byte[] expected = {0, (byte)TileFactory.FARM, FarmTile.WATERED, 0, 0, 0, 0, 15};
+		FarmTile tile = new FarmTile();
+		tile.till();
+		tile.water();
+		
+		PersistenceTile entry = PersistenceTile.fromTile(tile, 15);
+		ByteArrayOutputStream bo = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(bo);
+		try {
+			entry.toBinary(dos);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		byte[] bytes = bo.toByteArray();
+		assertArrayEquals(expected, bytes);
+	}
+	
+	@Test
+	public void testFromBinary() {
+		byte[] input = {0, (byte)TileFactory.FARM, FarmTile.WATERED, 0, 0, 0, 0, 15};
+		ByteArrayInputStream bi = new ByteArrayInputStream(input);
+		DataInputStream dis = new DataInputStream(bi);
+		try {
+			PersistenceTile persistence = PersistenceTile.fromBinary(dis);
+			FarmTile tile = (FarmTile)persistence.createTile();
+			assertEquals("Watered", tile.getName());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+}

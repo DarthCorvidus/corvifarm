da143795033d16d51b618d56b6a9c8efa4f8407d
Claus-Christoph Küthe <github@vm01.telton.de>
2024-06-27 22:07:54 +0200

    Added toBinary to Item/ItemAbstract.

--- src/main/java/com/corvidus/corvifarm/items/Item.java
+++ src/main/java/com/corvidus/corvifarm/items/Item.java
@@ -1,4 +1,8 @@
 package com.corvidus.corvifarm.items;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public interface Item {
 	public String getName();
 	public int getBaseDemand();
@@ -9,4 +13,5 @@ public interface Item {
 	public int getId();
 	public byte[] getModifiers();
 	public void setModifiers(byte[] mods);
+	public void toBinary(DataOutputStream dos) throws IOException;
 }
--- src/main/java/com/corvidus/corvifarm/items/ItemAbstract.java
+++ src/main/java/com/corvidus/corvifarm/items/ItemAbstract.java
@@ -1,9 +1,23 @@
 package com.corvidus.corvifarm.items;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public abstract class ItemAbstract implements Item {
 	static int NONE = 0;
 	private int amount = 1;
 	@Override
 	public abstract int getBaseDemand();
+	
+	@Override
+	public void toBinary(DataOutputStream dos) throws IOException {
+		byte[] modifiers = this.getModifiers();
+		dos.writeInt(this.getId());
+		dos.writeShort(this.amount);
+		for(int i = 0; i<4; i++) {
+			dos.writeByte(modifiers[i]);
+		}
+	}
 	
 	@Override
 	public int getDemand() {
